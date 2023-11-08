9eda54e6a183f17676143097f208a4f44306935c
Idowu Favour <favouridowu02@gmail.com>
2023-11-07 16:39:03 -0800

    a function that adds 2 tuples.

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,6 +3,4 @@ def multiple_returns(sentence):
     """a function that returns a tuple with the length of a
     string and its first character.
     """
-    if not sentence:
-        sentence[0] = None
-    return len(sentence), sentence[0]
+    return len(sentence), sentence[0] if sentence else None
