e25b600df0c518ea4936ee08bad9eafc385ceceb
Idowu Favour <favouridowu02@gmail.com>
2023-11-07 20:20:33 -0800

    a function in C that checks if a singly linked list is a palindrome.

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,4 +1,6 @@
 #include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * is_palindrome - a function in C that checks if a singly linked
@@ -8,29 +10,43 @@
  */
 int is_palindrome(listint_t **head)
 {
-	int table[1024];
+	int *table;
 	listint_t *current;
 	int i = 0;
 	int check = 0;
+	int k;
 
 	current = *head;
+	while (current != NULL)
+	{
+		i++;
+		current = current->next;
+	}
+	table = malloc(sizeof(int) * i);
+	if (table == NULL)
+	{
+		return 0;
+	}
+	current = *head;
+	i = 0;
 	while (current != NULL)
 	{
 		table[i] = current->n;
 		i++;
 		current = current->next;
 	}
-	if (i % 2 == 0)
-	{
-		int k = i / 2;
+	k = i / 2;
 
-		for (int n = 0; n <= k; n++, i--)
+	for (int n = 0; n < k; n++, i--)
+	{
+		if (table[n] == table[i - 1])
+			check = 1;
+		else
 		{
-			if (table[n] == table[i])
-				check = 1;
-			else
-				check = 0;
+			check = 0;
+			break;
 		}
 	}
+	free(table);
 	return (check);
 }
