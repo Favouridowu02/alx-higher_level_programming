559bb4e86a4835af080aaa53d9c9c117f1ab4751
Idowu Favour <favouridowu02@gmail.com>
2023-11-10 20:52:36 -0800

    Just finshed task 14

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-for i in range(26):
-    if i % 2 == 0:
-        print('{:c}'.format(122 - i), end='')
-    else:
-        print('{:c}'.format(90 - i), end='')
+for n in range(122, 96, -1):
+    print("{}".format(chr(n - 32) if n % 2 else chr(n)), end="")
