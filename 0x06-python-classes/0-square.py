9b4f422467b01f4e11669414ff01c2b9c0612ec2
Idowu Favour <favouridowu02@gmail.com>
2023-11-22 16:54:58 -0800

    Just completed task 0

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" This module contains a function that defines a square:"""
+class Square:
+    """ This is an empty class
+        the pass statement is used
+    """
+    pass
