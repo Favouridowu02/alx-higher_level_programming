1c650271870efaa579509422e7c0d05b639fdc2d
Idowu Favour <favouridowu02@gmail.com>
2023-11-22 17:13:22 -0800

    Just finished task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" This module contains a function that defines a square:"""
+
+
+class Square:
+    """ This class defines a Square
+
+        Attributes:
+            size: A Private instance attribute
+        the pass statement is used
+    """
+    def __init__(self, size):
+        self.__size = size
