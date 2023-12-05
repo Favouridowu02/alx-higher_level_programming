917c4482ac18f46da6d282bc665e8abd598e1a9f
root <root@bukola-idowu>
2023-12-05 23:17:44 +0100

    just completed task 101

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -2,6 +2,8 @@
 """ This moduule contains a class which only gives the previledge of using
 the first_name attribute using __slots__
 """
+
+
 class LockedClass:
     """  prevents the user from dynamically creating new instance attributes
     except if the new instance attribute is called first_name.
