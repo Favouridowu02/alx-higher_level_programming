845ac76b9c881d96a8fad08e7a94be5bd750e063
root <root@bukola-idowu>
2023-12-14 16:06:06 +0100

    Jut made some fixs

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -5,13 +5,12 @@
 
 
 def say_my_name(first_name, last_name=""):
-    """ 
+    """
         This function prints the user first name and last name
-        
+
         Arguments:
             first_name: the first parameter contains the user first name
             last_name: the last parameter contains the user last name
-
         Return: None
     """
 
@@ -19,5 +18,5 @@ def say_my_name(first_name, last_name=""):
         raise TypeError("first_name must be a string")
     if type(last_name) is not str:
         raise TypeError("last_name must be a string")
-    
+
     print("My name is {} {}".format(first_name, last_name))
