fff0b7fa122ce06f8a134b2f52f410b699ac0dd3
root <root@bukola-idowu>
2023-12-14 17:54:46 +0100

    Just finished task 3

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -7,7 +7,7 @@
 def print_square(size):
     """
     This function print a square with the character #
-    
+
     Arguments:
         size: the size length of the square
 
@@ -20,6 +20,6 @@ def print_square(size):
         raise ValueError("size must be >= 0")
     if size is float and size < 0:
         raise TypeError("size must be an integer")
-    
+
     for i in range(int(size)):
         print("#" * size, end="\n")
