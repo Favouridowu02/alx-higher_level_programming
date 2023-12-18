8cf87e9b74f47f7f2397abf859e13b3a070bc714
root <root@bukola-idowu>
2023-12-18 01:32:47 +0100

    Just completed task 10

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -18,9 +18,9 @@ class Square(Rectangle):
 
             Return: None
         """
-        self.__size = size
-        super().__init__(size, size)
         self.integer_validator("size", self.__size)
+        super().__init__(size, size)
+        self.__size = size
 
     def area(self):
         """ This Function returns the area of the square """
