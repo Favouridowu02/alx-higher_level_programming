cc61d7a4b9147df6b28eb13b8fccf9e3bd771bc0
root <root@bukola-idowu>
2024-02-02 21:05:34 +0100

    Just completed task 2

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+    This module contains a function that writes a string to a text file and returns the number of characters written
+"""
+
+
+def write_file(filename="", text=""):
+    with open(filename, mode="w", encoding="utf-8") as f:
+        f.write(text)
+    return len(text)
