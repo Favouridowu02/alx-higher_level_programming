c65f468d88e0d04cbe0ecdecc84539172aa69ada
root <root@bukola-idowu>
2024-02-02 20:51:51 +0100

    Just made some tweaks

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python3
+"""
+    This module contains a function that reads a text file in utf-8
+    and prints it to the stdout
+"""
+
+
 def read_file(filename=""):
-    with open(filename, mode="r", encoding = "utf-8") as f:
+    """
+        This function reads a text file in utf-8 and prints it to the std
+        output
+    """
+    with open(filename, mode="r", encoding="utf-8") as f:
         print(f.read())
