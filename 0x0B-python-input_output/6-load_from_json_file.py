0dbc744bee2f4d000c82102163a68360babccd30
root <root@bukola-idowu>
2024-02-14 04:11:36 +0100

    Just added the json file

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+    This module contains a function that creates an Object from a
+    “JSON file”
+"""
+
+
+def load_from_json_file(filename):
+    """
+        This function creates an Object from a “JSON file”
+    """
+    import json
+    with open(filename, "r+", encoding="utf-8") as f:
+        f.read()
