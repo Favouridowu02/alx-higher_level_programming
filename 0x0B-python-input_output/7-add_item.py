d2f054cc0e2bc40be95694d85329604c73eba32a
root <root@bukola-idowu>
2024-02-17 20:05:47 +0100

    Just made some tweaks

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,3 +1,22 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 """
-    
+   This module contains a script that adds all arguments to a Python
+   list, and then save them to a file
+"""
+
+filename = "add_json.py"
+
+from sys import argv
+
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+
+try:
+    text = load_from_json_file(argv)
+except:
+    text = []
+
+for arg in argv:
+    text.append(arg)
+
+save_to_json_file(text, "add_json")
