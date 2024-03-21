f6146a005ccd55cccf900627c0b586fa98ca403a
Favour Idowu <favouridowu02@gmail.com>
2024-03-21 10:41:34 +0100

    Added a script for ordering by score in the second_table while list

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,2 +1,2 @@
 -- A Script that lists all records of the table second_table of the database
-SELECT id, name FROM second_table ORDER BY score DESC;
+SELECT score, name FROM second_table ORDER BY score DESC;
