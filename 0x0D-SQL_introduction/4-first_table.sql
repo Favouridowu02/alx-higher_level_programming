ba6ec51472a9c7dde4fd494be764e7cbab795f39
Favour Idowu <favouridowu02@gmail.com>
2024-03-21 09:10:58 +0100

    Just created a script that creates a table with the id and name title

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,2 +1,2 @@
 -- A script that creates a table called first_table with id INT name VARCHAR(256)
-CREATE TABLE IF NOT EXISTS `first_name`(`id` INT, `name` VARCHAR(256));
+CREATE TABLE IF NOT EXISTS `first_name` (`id` INT, `name` VARCHAR(256));
