40b196b9e6adb3afc1da2df95d707d6fd1773e9d
Favour Idowu <favouridowu02@gmail.com>
2024-03-21 10:57:13 +0100

    Added a script for listing rows in second_table that are greater than 10

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,4 +1,4 @@
 -- A Script that lists all records with a score >= 10 in the table second_table
-SELECT * FROM `second_table`
+SELECT scores, name FROM `second_table`
 ORDER BY scores DESC
 where score >= 10;
