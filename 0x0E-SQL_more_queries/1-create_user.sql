7c2692202662173973ec9a5d243a404b37e4d2d7
Favour Idowu <favouridowu02@gmail.com>
2024-03-25 21:29:21 +0100

    Created a script that creates a user and assigns priveledges to it with a password

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
 -- A script to create a user with all priviledges
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES *.* to 'user_0d_1'@'localhost';
+GRANT ALL ON *.* to 'user_0d_1'@'localhost';
