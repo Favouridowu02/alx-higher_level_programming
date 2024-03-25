cce52bd13253db82206a5c2ab00b9ed7f616a913
Favour Idowu <favouridowu02@gmail.com>
2024-03-25 22:18:17 +0100

    Added a script ton creat a database and a script

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,6 +1,6 @@
 -- A Script that creates a database and a table states
-CREATE DATABASE hbtn_0d_usa;
-CREATE TABLE htbn_0d_usa.states(
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS htbn_0d_usa.states(
 	id INT NOT NULL AUTO_INCREMENT,
 	name VARCHAR(256)) NOT NULL,
 	PRIMARY KEY(id)
