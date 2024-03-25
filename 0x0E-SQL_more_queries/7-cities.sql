40c283c02fc81f0146c29958f085230038559535
Favour Idowu <favouridowu02@gmail.com>
2024-03-25 22:40:11 +0100

    Added a script to create a database and a script

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,9 +1,9 @@
 -- A script to create database and tables
-CREATE DATABASE hbtn_0d_usa;
-CREATE TABLE hbtn_0d_usa.cities (
-	id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
-	state_id INT NOT NULL,
-	name VARCHAR(256) NOT NULL,
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`cities` (
+	`id` INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
+	`state_id` INT NOT NULL,
+	`name` VARCHAR(256) NOT NULL,
 	FOREIGN KEY(`state_id`)
-	REFERENCES hbtn_0d_usa.states(id)
+	REFERENCES `hbtn_0d_usa`.`states`(`id`)
 );
