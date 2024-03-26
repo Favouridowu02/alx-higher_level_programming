834592efd8e92d0fa172184f71802acfb4105a61
Favour Idowu <favouridowu02@gmail.com>
2024-03-26 03:12:38 +0100

    added a script for listing a cities

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,3 +1,4 @@
 -- A Script that lists all cities contained in the database
 SELECT cities.id, cities.name, states.name FROM cities, states
+WHERE `state`.`id` = `cities`.`state_id`
 ORDER BY cities.id ASC;
