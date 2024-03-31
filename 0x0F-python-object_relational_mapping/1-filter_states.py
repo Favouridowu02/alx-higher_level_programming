2a05186e8cf15a7972b48186a211e9007c34d55d
Favour Idowu <favouridowu02@gmail.com>
2024-03-31 19:29:03 +0100

    Completed task 1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,25 +1,29 @@
 #!/usr/bin/python3
-from sys import argv
 
-import MySQLdb
+if __name__ == "__main__":
+    from sys import argv
 
-# Ensure the arguments a 3"
-if len(argv) != 4:
-    print("Usage: python 0-select_states.py username password dbname")
-    exit(1)
+    import MySQLdb
 
-db = MySQLdb.connect(
-        host="localhost",
-        user=argv[1],
-        passwd=argv[2],
-        db=argv[3],
-        port=3306)
 
-c = db.cursor()
-c.execute("SELECT id, name FROM states WHERE name LIKE 'N%' ORDER BY id")
-rows = c.fetchall()
-for row in rows:
-    print(row)
+    # Ensure the arguments a 3"
+    if len(argv) != 4:
+        print("Usage: python 0-select_states.py username password dbname")
+        exit(1)
 
-c.close()
-db.close()
+    db = MySQLdb.connect(
+            host="localhost",
+            user=argv[1],
+            passwd=argv[2],
+            db=argv[3],
+            port=3306)
+
+    c = db.cursor()
+    c.execute("SELECT id, name FROM states WHERE name LIKE 'N%' \
+              ORDER BY id")
+    rows = c.fetchall()
+    for row in rows:
+        print(row)
+
+    c.close()
+    db.close()
