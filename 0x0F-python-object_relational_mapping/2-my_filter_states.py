58caff09aed57690d74c2e67382eda3192c05d32
Favour Idowu <favouridowu02@gmail.com>
2024-03-31 21:38:30 +0100

    Completed task 2

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+    This module takes in four argument and displays all values
+    in the states table of hbtn_0e_0_usa where name matches the
+    argument
+"""
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+            user=argv[1],
+            passwd=argv[2],
+            db=argv[3],
+            host="localhost",
+            port=3306);
+    c = db.cursor()
+    c.execute("SELECT id, name FROM states WHERE name = '{}' ORDER BY id".format(argv[4]))
+
+    rows = c.fetchall()
+    for row in rows:
+        print(row)
+    c.close()
+    db.close()
