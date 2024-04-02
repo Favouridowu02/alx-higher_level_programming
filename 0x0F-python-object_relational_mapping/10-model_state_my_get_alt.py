b7cbeb713fb44926db75022bf59136bdda27d13c
Favour Idowu <favouridowu02@gmail.com>
2024-04-02 13:34:28 +0100

    a script that prints the State object with the name passed as argument from the database hbtn_0e_6_usa

--- 0x0F-python-object_relational_mapping/10-model_state_my_get_alt.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get_alt.py
@@ -13,10 +13,10 @@ if __name__ == "__main__":
                            .format(argv[1], argv[2], argv[3]))
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
-    session = Session()
-
+    session=Session()
+    
     substring = argv[4]
-    row = session.query(State).filter(State.name == (argv[4],))
+    row = session.query(State).filter(State.name.like(f"%{substring}%"))
     if row is None:
         print("Not found")
     else:
