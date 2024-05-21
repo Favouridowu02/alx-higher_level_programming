eee2487821f3c5487d50f2bf0cdc4409426fc61d
Favour Idowu <favouridowu02@gmail.com>
2024-05-21 02:36:40 +0100

    A script that prints a square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,9 +1,13 @@
 #!/usr/bin/node
 const { argv } = require('node:process');
 const length = argv[2];
-for (let i = 0; i < length; i++) {
-  for (let j = 0; j < length; j++) {
-    process.stdout.write('X');
+if (argv[2] === undefined || isNaN(argv[2]) === true) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < length; i++) {
+    for (let j = 0; j < length; j++) {
+      process.stdout.write('X');
+    }
+    console.log();
   }
-  console.log();
 }
