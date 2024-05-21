c0346894d968f83039f3da7c71ba6f6e4b5cc707
Favour Idowu <favouridowu02@gmail.com>
2024-05-21 01:30:29 +0100

    Just wrote a script that prints two arguments in the is format

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const { argv } = require("node:process");
+const { argv } = require('node:process');
 if (argv.length >= 2) {
-  console.log(argv[2] + " is " + argv[3])
+  console.log(argv[2] + ' is ' + argv[3]);
 }
