2e22ec4ab82e8df6d6285365a5be409714719b0d
Favour Idowu <favouridowu02@gmail.com>
2024-05-21 02:06:50 +0100

    A Script that prints x times 'C is fun'

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const { argv } = require('node:process');
 if (argv[2] === undefined) {
-  console.log('Missing number of occurrences')
+  console.log('Missing number of occurrences');
 } else if (isNaN(argv[2]) === false) {
   const lineNumber = argv[2];
   for (let i = 0; i < lineNumber; i++) {
