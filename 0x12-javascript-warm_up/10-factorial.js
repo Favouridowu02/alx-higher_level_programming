4e339af4155d58595b618e00ff33d82d7b38fbdd
Favour Idowu <favouridowu02@gmail.com>
2024-05-21 03:13:40 +0100

    A script that frints the factorial of a number passed as an argument

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const { argv } = require('node:process');
+if (argv[2] === undefined || isNaN(argv[2]) === true) {
+  console.log(1);
+} else {
+  function fact(num) {
+    if (num === 1 || num === 0) {
+  	  return (1);
+    }
+    return (num * fact(num - 1));
+  }
+  console.log(fact(argv[2]));
+}
