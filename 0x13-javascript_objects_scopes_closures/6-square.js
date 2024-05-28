9716745a9e4cc7878e0a5919076b46d495e6dee2
Favour Idowu <favouridowu02@gmail.com>
2024-05-28 20:50:07 +0100

    Just completed the task of inheriting the Square Class

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+const Sq = require('./5-square.js');
+class Square extends Sq {
+  constructor (size) {
+    super(size, size);
+    this.size = size;
+  }
+
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) {
+        process.stdout.write(c);
+      }
+      console.log();
+    }
+  }
+}
+module.exports = Square;
