bf78886ee687ba181977de965c5e252b166d2d73
Favour Idowu <favouridowu02@gmail.com>
2024-06-04 07:14:01 +0100

    A script that computes the number of tasks completed by user id.

--- 0x14-javascript-web_scraping/6-completed_tasks_alt.js
+++ 0x14-javascript-web_scraping/6-completed_tasks_alt.js
@@ -7,14 +7,17 @@ request(uri, (error, response, body) => {
     return error;
   }
   const newObj = {};
-  const todos = JSON.parse(body);
+  body = JSON.parse(body);
   const count = body.length;
-  todos.forEach((todo) => {
-    if (todo.completed && newObj[todo.userId] === undefined) {
-      newObj[todo.userId] = 1;
-    } else if (newObj[todo.userId]) {
-      newObj[todo.userId] += 1;
+  for (let i = 0; i < count; i++) {
+    let id = body[i].userId;
+    if (body[i].completed === true) {
+      if (!newObj[id]) {
+        newObj[id] = 1;
+      } else {
+        newObj[id] += 1;
+      }
     }
-  });
-  console.log(newObj);
+  }
+  console.log(JSON.stringify(newObj));
 });
