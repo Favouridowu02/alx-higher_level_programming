e8b04b9903cb019893dad0bb64d67380e28d974a
Favour Idowu <favouridowu02@gmail.com>
2024-06-04 07:15:47 +0100

    A script that computes the number of tasks completed by user id.

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,20 +1,16 @@
 #!/usr/bin/node
-const { argv } = require('node:process');
 const request = require('request');
-const uri = 'https://jsonplaceholder.typicode.com/todos';
-request(uri, (error, response, body) => {
-  if (error) {
-    return error;
+request(process.argv[2], function (error, response, body) {
+  if (!error) {
+    const todos = JSON.parse(body);
+    const completed = {};
+    todos.forEach((todo) => {
+      if (todo.completed && completed[todo.userId] === undefined) {
+        completed[todo.userId] = 1;
+      } else if (todo.completed) {
+        completed[todo.userId] += 1;
+      }
+    });
+    console.log(completed);
   }
-  const newObj = {};
-  const todos = JSON.parse(body);
-  const count = body.length;
-  todos.forEach((todo) => {
-    if (todo.completed && newObj[todo.userId] === undefined) {
-      newObj[todo.userId] = 1;
-    } else if (newObj[todo.userId]) {
-      newObj[todo.userId] += 1;
-    }
-  });
-  console.log(newObj);
 });
