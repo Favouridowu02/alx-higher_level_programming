9c4e939ffd95790bcf598224d33a2193f023cc37
Favour Idowu <favouridowu02@gmail.com>
2024-07-02 22:31:09 +0100

    A script that fetches and prints how to say “Hello” depending on the language

---
+++ 0x15-javascript-web_jquery/102-script.js
@@ -0,0 +1,10 @@
+const $ = window.$;
+$('document').ready(function () {
+  const url = 'https://hellosalut.stefanbohacek.dev/?lang=';
+  $('INPUT#btn_translate').click(function () {
+    const value = $('INPUT#language_code').val();
+    $.get(url + value, function (data, textStatus) {
+      $('DIV#hello').text(data.hello);
+    });
+  });
+});
